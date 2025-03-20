2679f8ecb02b0484e1c30ca646c7ffd777cae470
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-03-20 19:51:37 -0300

    Update routes to use Livewire components for Home and About
    Replaced the welcome view with the Home Livewire component and added a new route for the About Livewire component. This enhances the application's structure and leverages Livewire for dynamic content rendering.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Livewire\About;
+use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class)->name('home');
+
+Route::get('/about', About::class)->name('about');
 
 Route::middleware([
     'auth:sanctum',
